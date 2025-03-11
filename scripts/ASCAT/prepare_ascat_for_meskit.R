1804fafdbf1f7172bfdb2bd03c33e686a423795b
Jacqueline Marcia Boccacino <jb62@sanger.ac.uk>
2025-03-11 14:56:07 +0000

    Adapted scripts to deal with ploidy values and adjust copy number results

--- scripts/ASCAT/prepare_ascat_for_meskit.R
+++ scripts/ASCAT/prepare_ascat_for_meskit.R
@@ -1,20 +1,35 @@
 library(optparse)
 library(dplyr)
+library(readr)
 
 option_list <- list(
     make_option(c("-s", "--segments"),
         dest = "segments",
-        action = "store", 
-        default = NA, 
-        type = "character", 
+        action = "store",
+        default = NA,
+        type = "character",
         help = "Path to segments file."
-    ), 
+    ),
     make_option(c("-o", "--outdir"),
         dest = "outdir",
-        action = "store", 
-        default = NA, 
-        type = "character", 
+        action = "store",
+        default = NA,
+        type = "character",
         help = "Path to output directory."
+    ),
+    make_option(c("--prefix"),
+        dest = "prefix",
+        action = "store",
+        default = "",
+        type = "character",
+        help = "Prefix for output segment file."
+    ),
+    make_option(c("--ploidy"),
+        dest = "ploidy_file",
+        action = "store",
+        default = NA,
+        type = "character",
+        help = "Path to a file containing each sample's ploidy; if provided, copy number results will be normalised relative to the ploidy of each sample."
     )
 )
 
@@ -26,8 +41,17 @@ arguments <- parse_args(parser, positional_arguments = 0)
 
 segments <- arguments$options$segments
 outdir <- arguments$options$outdir
+ploidy_file <- arguments$options$ploidy_file
 
-# Read segments file.
+if (!is.na(ploidy)) {
+    ploidy_table <- readr::read_table(ploidy_file, col_names = FALSE)
+    colnames(ploidy_table) <- c("Tumor_Sample_Barcode", "Purity", "Ploidy")
+    warning("A ploidy table has been provided. Copy number results will be adjusted relative to the ploidy values!")
+} else {
+    warning("A ploidy table has not been provided. Proceeding without normalising copy number results...")
+}
+
+#  Read segments file.
 seg <- read.table(segments, header = TRUE, row.names = NULL)
 colnames(seg) <- c(
     "Tumor_Sample_Barcode",
@@ -61,9 +85,30 @@ seg <- seg %>% select(
     Size
 )
 
+if (!is.na(ploidy_file)) {
+    seg <- seg |>
+        dplyr::left_join(
+            ploidy_table,
+            by = dplyr::join_by(Tumor_Sample_Barcode)
+        ) |>
+        dplyr::mutate(Ploidy = round(Ploidy)) |>
+        dplyr::mutate(
+            CopyNumber_adjusted = dplyr::case_when(
+                CopyNumber == 0 ~ 0, # Deletion
+                CopyNumber < Ploidsy ~ 1, #  Loss
+                CopyNumber == Ploidy ~ 2, # Neutral
+                CopyNumber > Ploidy & CopyNumber < (2 * Ploidy) ~ 3, # Gain
+                CopyNumber >= (2 * Ploidy) ~ 4 #  Amplification
+            ),
+            .before = CopyNumber
+        ) |>
+        dplyr::select(!c(CopyNumber, Ploidy)) |>
+        dplyr::rename(CopyNumber = CopyNumber_adjusted)
+}
+
 write.table(
-    seg, 
-    file = paste0(outdir, "/segments_for_meskit.tsv"),
+    seg,
+    file = paste0(outdir, "/", prefix, "_segments_for_meskit.tsv"),
     sep = "\t",
     quote = FALSE,
     row.names = FALSE,
