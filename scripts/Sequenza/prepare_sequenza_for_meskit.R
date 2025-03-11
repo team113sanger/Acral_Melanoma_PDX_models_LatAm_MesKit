e57e5a9bd7b6d7d4864c52cc4411104ccf60449f
Jacqueline Marcia Boccacino <jb62@sanger.ac.uk>
2025-03-11 10:34:54 +0000

    Added option to normalise CopyNumber values relative to the ploidy of the samples

--- scripts/Sequenza/prepare_sequenza_for_meskit.R
+++ scripts/Sequenza/prepare_sequenza_for_meskit.R
@@ -6,29 +6,36 @@ library(fs)
 option_list <- list(
     make_option(c("--human"),
         dest = "human",
-        action = "store", 
-        default = NA, 
-        type = "character", 
+        action = "store",
+        default = NA,
+        type = "character",
         help = "Path to directory with Sequenza outputs for human tumours."
-    ), 
+    ),
     make_option(c("--pdx"),
         dest = "pdx",
-        action = "store", 
-        default = NA, 
-        type = "character", 
+        action = "store",
+        default = NA,
+        type = "character",
         help = "Path to directory with Sequenza outputs for PDX samples."
-    ), 
+    ),
     make_option(c("-o", "--outdir"),
         dest = "outdir",
-        action = "store", 
-        default = NA, 
-        type = "character", 
+        action = "store",
+        default = NA,
+        type = "character",
         help = "Path to output directory."
+    ),
+    make_option(c("--ploidy"),
+        dest = "ploidy",
+        action = "store",
+        default = NA,
+        type = "character",
+        help = "Path to a file containing each sample's ploidy; if provided, copy number results will be normalised relative to the ploidy of each sample."
     )
 )
 
 parser <- OptionParser(
-    usage = "prepare_sequenza_for_meskit.R [options] --human path/to/human --pdx path/to/pdx --outdir path/to/outdir",
+    usage = "prepare_sequenza_for_meskit.R [options] --human path/to/human --pdx path/to/pdx --outdir path/to/outdir --ploidy path/to/ploidy.tsv",
     option_list = option_list
 )
 arguments <- parse_args(parser, positional_arguments = 0)
@@ -36,9 +43,15 @@ arguments <- parse_args(parser, positional_arguments = 0)
 human <- arguments$options$human
 pdx <- arguments$options$pdx
 outdir <- arguments$options$outdir
+ploidy <- arguments$options$ploidy
 
 sequenza_files_human <- fs::dir_ls(human, recurse = TRUE, glob = "*_segments.txt$")
 sequenza_files_pdx <- fs::dir_ls(pdx, recurse = TRUE, glob = "*_segments.txt$")
+if (!is.na(ploidy)) {
+    ploidy_table <- readr::read_table(ploidy) |> dplyr::rename(Tumor_Sample_Barcode = sample)
+} else {
+    warning("A ploidy table has not been provided. Proceeding without normalising copy number results...")
+}
 
 seg_human <- readr::read_tsv(sequenza_files_human, col_names = TRUE, id = "Tumor_Sample_Barcode") |>
     dplyr::mutate(
@@ -64,6 +77,20 @@ seg_human <- readr::read_tsv(sequenza_files_human, col_names = TRUE, id = "Tumor
         Minor_CN
     )
 
+if (!is.na(ploidy)) {
+    seg_human <- seg_human |>
+        dplyr::left_join(
+            ploidy_table,
+            by = dplyr::join_by(Tumor_Sample_Barcode)
+        ) |>
+        dplyr::mutate(
+            CopyNumber_normalised = round(CopyNumber / ploidy.mean.cn),
+            .before = CopyNumber
+        ) |>
+        dplyr::select(!c(CopyNumber, ploidy.mean.cn)) |>
+        dplyr::rename(CopyNumber = CopyNumber_normalised)
+}
+
 seg_pdx <- readr::read_tsv(sequenza_files_pdx, col_names = TRUE, id = "Tumor_Sample_Barcode") |>
     dplyr::mutate(
         Tumor_Sample_Barcode = stringr::str_remove(basename(Tumor_Sample_Barcode), "_segments.txt"),
@@ -86,15 +113,29 @@ seg_pdx <- readr::read_tsv(sequenza_files_pdx, col_names = TRUE, id = "Tumor_Sam
         CopyNumber,
         Major_CN,
         Minor_CN
-    ) |>
-    dplyr::mutate(
-        Tumor_Sample_Barcode = paste0(Tumor_Sample_Barcode, "_hum")
     )
+
+if (!is.na(ploidy)) {
+    seg_pdx <- seg_pdx |>
+        dplyr::left_join(
+            ploidy_table,
+            by = dplyr::join_by(Tumor_Sample_Barcode)
+        ) |>
+        dplyr::mutate(
+            CopyNumber_normalised = round(CopyNumber / ploidy.mean.cn),
+            .before = CopyNumber
+        ) |>
+        dplyr::select(!c(CopyNumber, ploidy.mean.cn)) |>
+        dplyr::rename(CopyNumber = CopyNumber_normalised) |>
+        dplyr::mutate(
+            Tumor_Sample_Barcode = paste0(Tumor_Sample_Barcode, "_hum")
+        )
+}
 
 seg <- dplyr::bind_rows(seg_human, seg_pdx) |> tidyr::drop_na()
 
 write.table(
-    seg, 
+    seg,
     file = paste0(outdir, "/segments_for_meskit.tsv"),
     sep = "\t",
     quote = FALSE,
